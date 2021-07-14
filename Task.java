62fbd4202ef5792ed037e60dfe5f1410238a12d8
Ioannis Fountos <42605477+Ewan10@users.noreply.github.com>
2021-07-14 16:35:33 +0300

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+
+class Task implements Serializable {
+    private String title;
+    private LocalDate date;
+    private LocalTime time;
+    private String status = "";
+
+    public Task(String title, LocalDate date, LocalTime time) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String s) {
+        title = title + s;
+    }   
+    public LocalDate getDate() {
+        return date;
+    } 
+    public void setDate(LocalDate s) {
+        date = s;
+    }
+    public void setTime(LocalTime t) {
+        time = t;
+    }
+    public void setStatus(String s) {
+        status = s;
+    }
+    public void print() { System.out.println("title: " + title + status + 
+                         " date: " + date + " time: " + time); 
+    }
+}
