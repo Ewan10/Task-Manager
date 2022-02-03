1b2e7dedb5b96b53ee30009baabffab23a0eab76
Ioannis Fountos <secgeorgiou@hotmail.com>
2022-02-03 13:31:59 +0200

    How save and load methods could implement LocalDateTypeAdapter

--- src/main/java/com/ewan/TaskManager/TaskManager.java
+++ src/main/java/com/ewan/TaskManager/TaskManager.java
@@ -5,12 +5,25 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.*;
 import java.lang.reflect.Type;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 class TaskManager {
   private final List<Task> taskStorage = new ArrayList<Task>();
+  
+  private Task stringToJodaTask(String title, String dateAsText, String timeAsText) {
+    DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy");
+    LocalDate parsedDate = LocalDate.parse(dateAsText,fmt);
+    LocalTime parsedTime = LocalTime.parse(timeAsText);
+    return new Task(title, parsedDate, parsedTime);
+  }
 
-  public void add(Task t) {
-     taskStorage.add(t);
+  public void add(String title, String dateAsText, String timeAsText) {
+    Task t = stringToJodaTask(title, dateAsText, timeAsText);
+
+    taskStorage.add(t);
   }
 
   public void viewAll() {
@@ -20,7 +33,7 @@ class TaskManager {
   }
 
   public void save(String fileName) {
-        Gson jsonObject = new Gson();
+        Gson jsonObject = new LocalDateTypeAdapter().getGson();
         String jsonObjectAsString = jsonObject.toJson(taskStorage);
 
         try {
@@ -29,22 +42,23 @@ class TaskManager {
             fileWriter.flush();
             fileWriter.close();
         }
+        
         catch(Exception e){
           e.printStackTrace();
           System.exit(1);
         }
   }
 
-  public void load(String fileName) {
-      String lines = "";
-      
+  public void load(String file) {
+  
+      String lines = "";  
       try {
          String oneLine = null;
-         BufferedReader buffRead = new BufferedReader(new FileReader(fileName));
+         BufferedReader buffRead = new BufferedReader(new FileReader(file));
          while((oneLine = buffRead.readLine())!= null ){
             lines += oneLine;
          }
-        buffRead.close();
+         buffRead.close();
       }
       catch(FileNotFoundException e) {
         e.printStackTrace();
@@ -56,7 +70,8 @@ class TaskManager {
       }
       //Use the string "lines" from the read file to recreate the list of tasks.
       Type listType = new TypeToken<ArrayList<Task>>(){}.getType();
-      List<Task> listOfTasks = new Gson().fromJson(lines, listType);
+      List<Task> listOfTasks = new LocalDateTypeAdapter().getGson()
+      .fromJson(lines, listType);
       taskStorage.addAll(listOfTasks);
   }
  }
